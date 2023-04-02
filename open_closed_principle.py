5440adab92aa3a3c14dfbd742628ea679524201b
vinayharyan28 <vinayharyan28@gmail.com>
2023-04-02 22:14:29 +0530

    add single responsibility and open closed principle

---
+++ open_closed_principle.py
@@ -0,0 +1,49 @@
+""" A class must be open to extension but closed to modification.
+    When we want to add new things to our model, we only want to add new things &
+    not change anything existing that is closed to modification.
+"""
+
+
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+
+class Tester(Employee):
+    def __init__(self, name, salary):
+        super().__init__(name, salary)
+
+    def test(self):
+        print(f'{self.name} is testing')
+
+
+class Developer(Employee):
+    def __init__(self, name, salary):
+        super().__init__(name, salary)
+
+    def develop(self):
+        print(f'{self.name} is developing')
+
+
+class Company:
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+    def work(employee):
+        if isinstance(employee, Developer):
+            employee.develop()
+        elif isinstance(employee, Tester):
+            employee.test()
+        else:
+            raise Exception('unknown employee')
+
+
+if __name__ == '__main__':
+    tester = Tester('Akshay', 5000)
+    developer = Developer('vinay', 5000)
+    company = Company('TATA')
+    company.work(tester)
+    company.work(developer)
+
---
+++ single_responsibility_principle.py
@@ -0,0 +1,17 @@
+# A class should have one, & only one reason to change
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'Person name (name={self.name})'
+
+    @staticmethod
+    def save(person):
+        print(f'save the {person} to database')
+
+
+if __name__ == '__main__':
+    person = Person('vinay')
+    person.save(person)
